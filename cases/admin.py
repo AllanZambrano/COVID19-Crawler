0cadd5bf15b50213622afde504f889545a7082cb
Allan Zambrano <allazambrano@gmail.com>
2020-03-22 21:35:42 -0300

    Change: Models. settings & admin

--- cases/admin.py
+++ cases/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Case
+from .models import Case, Entry, Country
 
 class CaseAdmin(admin.ModelAdmin):
-    list_display = ('country', 'total_cases', 'active_cases', 'total_deaths')
-    ordering = ['-total_cases']
+    list_display = ('country', 'confirmed', 'active', 'deaths')
+    ordering = ['-confirmed']
     search_fields = ['country']
 
-admin.site.register(Case, CaseAdmin)
 
+class EntryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Case, CaseAdmin)
+admin.site.register(Entry, EntryAdmin)
+admin.site.register(Country)
--- cases/models.py
+++ cases/models.py
@@ -1,10 +1,49 @@
 from django.db import models
 
+class Country(models.Model):
+    name = models.CharField(max_length=150, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+class Entry(models.Model):
+    case = models.ForeignKey('Case', on_delete=models.CASCADE)
+    new_cases = models.PositiveIntegerField(default=0)
+    new_deaths = models.PositiveIntegerField(default=0)
+    SOURCE_STATUS = (
+            ('WA', 'Waiting Approval'),
+            ('A', 'Approved'),
+            ('R', 'Rejected'),
+        )
+    status = models.CharField(
+        max_length=2,
+        choices=SOURCE_STATUS,
+        blank=True,
+        default='WA',
+        help_text='Source Status',
+    )
+    source = models.URLField()
+    comment = models.TextField(blank=True, null=True)
+    date = models.DateField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Entries"
+
+    def __str__(self):
+        return self.case.country
+
 class Case(models.Model):
-    country = models.CharField(max_length=150, primary_key=True)
-    total_cases = models.IntegerField()
-    active_cases = models.IntegerField()
-    total_deaths =  models.IntegerField()
+    country = models.ForeignKey('Country', on_delete=models.CASCADE)
+    confirmed = models.PositiveIntegerField(default=0)
+    active = models.PositiveIntegerField(default=0)
+    new_cases = models.PositiveIntegerField(default=0)
+    deaths =  models.PositiveIntegerField(default=0)
+    new_deaths = models.PositiveIntegerField(default=0)
+    last_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.country
