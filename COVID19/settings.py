886f4998cea823f463de9ef622741efff14b1ec7
Allan Zambrano <allazambrano@gmail.com>
2020-03-27 02:08:26 -0300

    Change setting, again

--- COVID19/settings.py
+++ COVID19/settings.py
@@ -137,7 +137,7 @@ if DEBUG == False:
     SECURE_REFERRER_POLICY = 'same-origin'
 
 # Heroku
+django_heroku.settings(locals())
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-django_heroku.settings(locals())
