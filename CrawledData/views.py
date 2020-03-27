719a0b61c7bc727d96d0193f2b1fea83de25ca84
Allan Zambrano <allazambrano@gmail.com>
2020-03-27 02:23:22 -0300

    Changes in view

--- CrawledData/views.py
+++ CrawledData/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import CrawlerCovid
 from django.views.generic import ListView, TemplateView
-from django.db.models import Sum
+
 
 class CrawledListView(ListView):
     model = CrawlerCovid
@@ -9,10 +9,3 @@ class CrawledListView(ListView):
 
 class Home(TemplateView):
     template_name = "home.html"
-    def get_context_data(self, **kwargs):
-        context = super(Home, self).get_context_data(**kwargs)
-        context['total'] = CrawlerCovid.objects.aggregate(sum_cases=Sum("confirmed"),
-                                                          sum_active=Sum("active"),
-                                                          sum_deaths=Sum("deaths"))
-        context['update'] = CrawlerCovid.objects.values("updated_at").order_by()[0]
-        return context
