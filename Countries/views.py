af875d9a3b194315711ac3cd24c7482e54d90965
Allan Zambrano <allazambrano@gmail.com>
2020-03-28 16:09:05 -0300

    Add: Cumulative graph

--- Countries/views.py
+++ Countries/views.py
@@ -1,13 +1,15 @@
 from .models import Country, CountryEntry
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
-from django.db.models import Sum, Count, Variance
+from django.db.models import Sum, Window, F
 from .forms import NewEntryForm
 from django.urls import reverse_lazy
+
 
 class CountryList(ListView):
     model = Country
     template_name = 'countrylist.html'
+
 
 class CountryDetail(DetailView):
     model = Country
@@ -21,7 +23,11 @@ class CountryDetail(DetailView):
             total_cases=Sum("new_cases"),
             total_deaths=Sum("new_deaths"),
         )
+        context['cum_sum'] = CountryEntry.objects.filter(country__name=self.object).annotate(
+            cumsum=Window(Sum('new_cases'), order_by=F("date").asc())).order_by("date",'cumsum').values("date", "cumsum").distinct()
+
         return context
+
 
 class NewEntry(CreateView):
     template_name = 'new_entry.html'
